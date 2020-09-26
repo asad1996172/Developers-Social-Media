01fedbc0487f41995917b3e58d4c7ae84d7deb0b
Asad Mahmood <asad1996172@hotmail.com>
2020-09-26 17:53:45 -0500

    Added regiter and auth apis

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const express = require('express');
 const connectDB = require('./config/db');
 
 const app = express();
-connectDB(); //connect databse
+
+// Connect databse
+connectDB(); 
+
+// Initialize middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res)=>{
     res.send('API Running');
