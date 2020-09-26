5f4acdd5d64bf1ea232580463481675a891bf5e3
Asad Mahmood <asad1996172@hotmail.com>
2020-09-26 16:16:13 -0500

    Added routes

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ app.get('/', (req, res)=>{
     res.send('API Running');
 });
 
+// Define Routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 const PORT = process.env.PORT || 5000; // look for default port else set to 5000 for local
 
 app.listen(PORT, ()=>{
