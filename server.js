460f2de57c36535a245bd83e6ef421c24a4e869c
Asad Mahmood <asad1996172@hotmail.com>
2020-09-26 15:41:07 -0500

    Initialized Repo

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.send('API Running');
+});
+
+const PORT = process.env.PORT || 5000; // look for default port else set to 5000 for local
+
+app.listen(PORT, ()=>{
+    console.log(`Server started on PORT ${PORT}`);
+});
