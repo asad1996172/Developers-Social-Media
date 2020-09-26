67f9b7e1add5b0cfcbef51220bd4cbd46d52808c
Asad Mahmood <asad1996172@hotmail.com>
2020-09-26 16:02:24 -0500

    Added mongodb connection code

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const config = require('config');
+
+const db = config.get('mongoURI');
+
+// Useful tips: Use try catch with async await
+
+const connectDB = async() => {
+    try{
+        await mongoose.connect(db ,{
+            useNewUrlParser:true,
+            useUnifiedTopology:true
+        });
+        console.log('MongoDB connected ...')
+    } catch (err){
+        console.error(err.message);
+        process.exit(1); // exit process with failure
+    }
+}
+
+module.exports = connectDB;
