8fa94193f1499c681bba79dd04428e548f4052cb
Asad Mahmood <asad1996172@hotmail.com>
2020-09-27 11:08:31 -0500

    Added profile routes, create update and delete

--- config/db.js
+++ config/db.js
@@ -7,10 +7,11 @@ const db = config.get('mongoURI');
 
 const connectDB = async() => {
     try{
-        await mongoose.connect(db ,{
-            useNewUrlParser:true,
-            useUnifiedTopology:true,
-            useCreateIndex: true
+        await mongoose.connect(db, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+          useCreateIndex: true,
+          useFindAndModify: false
         });
         console.log('MongoDB connected ...')
     } catch (err){
