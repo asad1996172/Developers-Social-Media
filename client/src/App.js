28796efe5f2213c538bd9a0706e45edbed80d5c0
Asad Mahmood <asad1996172@hotmail.com>
2020-10-27 19:05:35 -0500

    Auth done

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,49 @@
-import React, { Fragment } from 'react';
-import { Navbar } from './components/layout/Navbar';
+import React, { Fragment, useEffect } from 'react';
+import Navbar from './components/layout/Navbar';
 import { Landing } from './components/layout/Landing';
-import { Login } from './components/auth/Login';
+import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
-import{ BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
 
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
 // fragment is just a ghost element that doesn't show up in DOM
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path="/" component={Landing}></Route>
-        <section className="container">
-          <Alert />
-          <Switch>
-            <Route exact path="/register" component={Register}></Route>
-            <Route exact path="/login" component={Login}></Route>
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
-  </Provider>
-);
+const App = () => {
+  useEffect(() => {
+    console.log('Sending Dsipathc');
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path="/" component={Landing}></Route>
+          <section className="container">
+            <Alert />
+            <Switch>
+              <Route exact path="/register" component={Register}></Route>
+              <Route exact path="/login" component={Login}></Route>
+              <PrivateRoute exact path="/dashboard" component={Dashboard}></PrivateRoute>
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
