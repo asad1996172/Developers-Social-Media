217cab3b5fd269d8bf8b82b0932d791d6c9b6640
Asad Mahmood <asad1996172@hotmail.com>
2020-10-30 15:59:05 -0500

    Completed Comments and Posts

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,8 @@ import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
+import Post from './components/post/Post';
 import PrivateRoute from './components/routing/PrivateRoute';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
@@ -69,6 +71,16 @@ const App = () => {
                 exact
                 path="/add-education"
                 component={AddEducation}
+              ></PrivateRoute>
+              <PrivateRoute
+                exact
+                path="/posts"
+                component={Posts}
+              ></PrivateRoute>
+              <PrivateRoute
+                exact
+                path="/posts/:id"
+                component={Post}
               ></PrivateRoute>
             </Switch>
           </section>
