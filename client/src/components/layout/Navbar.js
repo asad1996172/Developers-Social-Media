07e7a4086ced90061ee8cd8c29a0ac01ebb83d63
Asad Mahmood <asad1996172@hotmail.com>
2020-10-31 11:33:24 -0500

    Finalizing things

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -51,7 +51,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     <nav className="navbar bg-dark">
       <h1>
         <Link to="/">
-          <i className="fas fa-code"></i> DevConnector
+          CodersMedia
         </Link>
       </h1>
       {!loading && (
