202455f91c590b049520c7d7e4f5af1e58a0b026
asad1996172 <asad007mahmood@gmail.com>
2023-10-12 19:51:33 -0700

    updated env vars

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -316,9 +316,7 @@ router.get('/github/:username', (req, res)=>{
     const options = {
       uri: `https://api.github.com/users/${
         req.params.username
-      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
-        'githubClientId'
-      )}&client_secret=${config.get('githubSecret')}`,
+        }/repos?per_page=5&sort=created:asc&client_id=${process.env.GITHUB_CLIENT_ID}&client_secret=${process.env.GITHUB_CLIENT_SECRET}`,
       method: 'GET',
       headers: {'user-agent': 'node.js'}
     };
